b153c68a3d3a4dea6c110f7cf152db410c12fa22
Rohit Bhanushali <rohitbhanushali11@gmail.com>
2025-05-31 01:15:06 +0530

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,26 @@
+/** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  allowedDevOrigins: ['*']
+  allowedDevOrigins: ['*'],
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ]
+  },
 }

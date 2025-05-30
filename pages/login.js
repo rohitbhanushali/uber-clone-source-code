d15949f8cd479d7bbcacd45200925b92d85a8b57
Rohit Bhanushali <rohitbhanushali11@gmail.com>
2025-05-30 22:41:34 +0530

    Initial commit

--- pages/login.js
+++ pages/login.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import tw from "tailwind-styled-components"
 import { useRouter } from "next/router";
 import { signInWithPopup, onAuthStateChanged } from 'firebase/auth'
@@ -6,6 +6,7 @@ import { auth, googleProvider } from '../firebase'
 
 export default function Login() {
     const router = useRouter()
+    const [error, setError] = useState('')
 
     useEffect(() => {
         onAuthStateChanged(auth, (user) => {
@@ -15,14 +16,25 @@ export default function Login() {
         });
     }, []) 
 
+    const handleSignIn = async () => {
+        try {
+            setError('')
+            await signInWithPopup(auth, googleProvider)
+        } catch (error) {
+            console.error('Login error:', error)
+            setError('Failed to sign in with Google. Please try again.')
+        }
+    }
+
     return (
         <Wrapper>
             <UberLogo src="https://i.ibb.co/n6LWQM4/Post.png"/>
             <Title>Log in to access your account.</Title>
             <HeadImage src="https://i.ibb.co/CsV9RYZ/login-image.png"/>
-            <SignInButton onClick={() => signInWithPopup(auth, googleProvider)}>
+            <SignInButton onClick={handleSignIn}>
                 Sign in with Google
             </SignInButton>
+            {error && <ErrorMessage>{error}</ErrorMessage>}
         </Wrapper>
     );
 }
@@ -34,6 +46,7 @@ flex flex-col h-screen w-screen bg-gray-200 p-4
 const SignInButton = tw.button`
 bg-black text-white text-center py-4 mt-8 self-center w-full
 `
+
 const UberLogo = tw.img`
 h-20 w-auto object-contain self-start
 `
@@ -44,4 +57,8 @@ text-5xl pt-4 text-gray-500
 
 const HeadImage = tw.img`
 w-auto object-contain
+`
+
+const ErrorMessage = tw.div`
+text-red-500 text-center mt-4
 `
